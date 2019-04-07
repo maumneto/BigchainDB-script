f4c1218ec053304e18f6b32192253ca43cd0a99e
sergio <sergio@sergio-pc.domain.name>
2019-04-07 14:25:33 -0300

    reset code with config file

--- bigchain-reset.sh
+++ bigchain-reset.sh
@@ -1,21 +1,31 @@
-echo "================= DROPPING BIGCHAIN DATABASE ================="
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+echo "${red}================= DROPPING BIGCHAIN DATABASE =================${reset}"
 bigchaindb -y drop
-echo "================= CLOSING BIGCHAINDB PROCESSES, IF THEY EXIST ================="
+echo "${red}================= CLOSING BIGCHAINDB PROCESSES, IF THEY EXIST =================${reset}"
 kill -9 $(ps aux | grep "bigchaindb" | awk '{print $2}')
-echo "================= STOPPING MONIT ================="
+echo "${red}================= STOPPING MONIT =================${reset}"
 monit quit
-echo "================= RESETTING TENDERMINT CONFIGURATIONS ================="
+echo "${red}================= RESETTING TENDERMINT CONFIGURATIONS =================${reset}"
 tendermint unsafe_reset_all
 tendermint unsafe_reset_priv_validator
-echo "================= DELETING TENDERMINT FOLDER ================="
+echo "${red}================= DELETING TENDERMINT FOLDER =================${reset}"
 rm -r -v $HOME/.tendermint/
-echo "================= CREATE TENDERMINT FOLDER ================="
+echo "${red}================= CREATE TENDERMINT FOLDER =================${reset}"
+file="config.toml"
+if [ -f "$file" ]
+then
+	echo "${green} $file found. Copying file to tendermint folder.${reset}"
+	mkdir -v $HOME/.tendermint
+	mkdir -v $HOME/.tendermint/config
+	cp -v config.toml $HOME/.tendermint/config/config.toml
+else
+	echo "${green} $file not found. Using default file${reset}"
+fi
 tendermint init
-echo -e "\n\n"
-echo "if you wish, it's time to change configs on tendermint folder ($HOME/.tendermint/config/config.toml)"
-read -p "press [ENTER] to continue..."
-echo -e "\n"
-echo "================= STARTING MONIT ================="
+echo "${red}================= STARTING MONIT =================${reset}"
 monit -d 1
-echo "================= STARTING BIGCHAINDB ================="
+echo "${red}================= STARTING BIGCHAINDB =================${reset}"
 bigchaindb start
